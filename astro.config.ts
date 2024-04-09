0b545aa3def78aa092aae0f0960f0ca05ff8ecea
mithrasu <quaizast@gmail.com>
2024-04-10 00:33:22 +0800

    ↩ revert: remove vercel configuration

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,6 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -40,11 +39,6 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
-  // Vercel Web Analytics
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
 });
 
 
