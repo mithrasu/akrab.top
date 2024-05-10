ab64170712cf76236ab9fc643291f07453d4919a
mithrasu <quaizast@gmail.com>
2024-05-10 16:13:42 +0800

    ✨ feat: add full-text rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,7 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
 import { SITE } from "@config";
+import { marked } from "marked";
 
 export async function GET() {
   const posts = await getCollection("blog");
@@ -10,11 +11,13 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, slug }) => ({
+    items: sortedPosts.map(({ data, slug, body }) => ({
       link: `posts/${slug}/`,
       title: data.title,
       description: data.description,
+      tags: data.tags,
       pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+      content: marked(body),
     })),
   });
 }
